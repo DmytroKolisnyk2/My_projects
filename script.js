8e05efbe3e6302d33dc527f80e091c318f5a641f
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-04-04 12:46:44 +0300

    add defaul markup

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const containerRef = document.querySelector('.card-container')
+import cards from "./cards.js";
+const containerRef = document.querySelector(".card-container");
 console.log(containerRef);
+console.log(cards);
+let cardsAmount = 16;
+function drawCards() {
+  let colection = [];
+  while (colection.length * 2 === cardsAmount) {
+    cards.forEach(card => {
+
+    })
+  }
+  // Math.round(Math.random() * cards.length);
+}
+drawCards();
