adb940d6e4c7edc7cddeb14a57cd8879fae17c97
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-04-07 21:24:42 +0300

    add site

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,5 @@
-const drawCards = function (amount, cards) {
+// генерує в випадкові послідовності картки (приймає кількість карток, масив карток та посилання куди вставити картки)
+export const drawCards = (amount, cards, containerRef) => {
   let collection = [];
   while (!(collection.length === amount)) {
     let random = Math.ceil(Math.random() * cards.length);
@@ -12,11 +13,57 @@ const drawCards = function (amount, cards) {
         random = Math.ceil(Math.random() * cards.length);
         if (setOfCards.includes(random)) {
           let card = cards.find((card) => card.id === random);
-          let string = `<div class="card card-${card.id}" style="background-color: ${card.color};"><img src="${card.src}" alt="${card.description}"></div>`;
-          cards.insertAdjacentHTML("beforeend", string);
+          let string = `<div class="card card-${card.id} default" data-id='${card.id}' style="background-color: ${card.color};"><img src="${card.src}" alt="${card.description}"></div>`;
+          containerRef.insertAdjacentHTML("beforeend", string);
           setOfCards.splice(setOfCards.indexOf(random), 1);
         }
       }
     }
   }
-}
+};
+
+// починає гру на заданих картках (приймає масив карток)
+export const gamePlay = (cards) => {
+  const state = { ref: "", id: 0, position: 1 };
+  const compareCard = (event) => {
+    if (state.position === 2) {
+      state.position = 1;
+      const repairCards = function () {
+        this.ref.classList.remove("active");
+        this.currentTarget.classList.remove("active");
+        cards.forEach((card) => card.addEventListener("click", compareCard));
+      };
+      if (event.currentTarget.dataset.id === state.id) {
+        event.currentTarget.removeEventListener("click", compareCard);
+        state.position = 1;
+        event.currentTarget.querySelector("img").classList.add("active");
+        event.currentTarget.classList.add("active");
+        // console.log(cardsRef.indexOf({ ...event.currentTarget }));
+        // cardsRef.splice(cardsRef.indexOf(event.currentTarget), 1);
+        // cardsRef.splice(state.ref.indexOf(event.currentTarget), 1);
+      }
+      if (event.currentTarget.dataset.id !== state.id) {
+        event.currentTarget.classList.add("active");
+        cards.forEach((card) => card.removeEventListener("click", compareCard));
+        state.currentTarget = event.currentTarget;
+        setTimeout(repairCards.bind(state), 500);
+      }
+    } else if (state.position === 1) {
+      state.position = 2;
+      state.ref = event.currentTarget;
+      state.id = event.currentTarget.dataset.id;
+      event.currentTarget.removeEventListener("click", compareCard);
+      event.currentTarget.querySelector("img").classList.add("active");
+      event.currentTarget.classList.add("active");
+    }
+  };
+  cards.forEach((card) => card.addEventListener("click", compareCard));
+};
+
+// розпочинає гру із заданими параметрами
+
+// export const startGame = (cardsAmount, cards, containerRef) => {
+//   drawCards(cardsAmount, cards, containerRef);
+//   const cardsRef = [...containerRef.querySelectorAll(".card")];
+//   gamePlay(cardsRef);
+// };
--- js/script.js
+++ js/script.js
@@ -1,73 +1,25 @@
 import cards from "./cards.js";
+import { drawCards } from "./functions.js";
+import { gamePlay } from "./functions.js";
+// import { startGame } from "./functions.js";
 const containerRef = document.querySelector(".card-container");
-const cardsAmount = 12;
-const drawCards = function (amount, cards) {
-  let collection = [];
-  while (!(collection.length === amount)) {
-    let random = Math.ceil(Math.random() * cards.length);
-    if (!collection.includes(random)) {
-      collection.push(random);
-    }
-    if (collection.length === amount / 2) {
-      collection = [...collection, ...collection];
-      let setOfCards = collection.slice();
-      while (!(setOfCards.length === 0)) {
-        random = Math.ceil(Math.random() * cards.length);
-        if (setOfCards.includes(random)) {
-          let card = cards.find((card) => card.id === random);
-          let string = `<div class="card card-${card.id} default" data-id='${card.id}' style="background-color: ${card.color};"><img src="${card.src}" alt="${card.description}"></div>`;
-          containerRef.insertAdjacentHTML("beforeend", string);
-          setOfCards.splice(setOfCards.indexOf(random), 1);
-        }
-      }
-    }
-  }
-};
-drawCards(cardsAmount, cards);
+let cardsAmount = 12;
+
+document
+  .querySelector(".count-cards__wrapper")
+  .addEventListener("click", () => alert("В розробці"));
+// .addEventListener("click", (event) => cardsAmount = +event.target.dataset.count);
+document
+  .querySelector(".start-btn")
+  .addEventListener("click", () => alert("В розробці"));
+// .addEventListener("click", startGame(cardsAmount, cards, containerRef));
+// function startGame(cardsAmount, cards, containerRef) {
+//   console.log('START');
+//   containerRef.querySelectorAll(".card").forEach(card => {card.remove});
+//   drawCards(cardsAmount, cards, containerRef);
+//   const cardsRef = [...containerRef.querySelectorAll(".card")];
+//   gamePlay(cardsRef);
+// };
+drawCards(cardsAmount, cards, containerRef);
 const cardsRef = [...containerRef.querySelectorAll(".card")];
-const gamePlay = function (cards) {
-  const state = { ref: "", id: 0, position: 1 };
-  const compareCard = function (event) {
-    if (state.position === 2) {
-      state.position = 1;
-      console.log(state);
-      console.log(cardsRef);
-      const repairCards = function () {
-        state.ref.classList.remove("active");
-        state.currentTarget.classList.remove("active");
-        state.ref.addEventListener("click", compareCard);
-        state.currentTarget.addEventListener("click", compareCard);
-      };
-      if (event.currentTarget.dataset.id === state.id) {
-        event.currentTarget.removeEventListener("click", compareCard);
-        state.position = 1;
-        console.log(event.currentTarget);
-        event.currentTarget.querySelector("img").classList.add("active");
-        event.currentTarget.classList.add("active");
-        console.log(1);
-        // console.log(cardsRef.indexOf({ ...event.currentTarget }));
-        // cardsRef.splice(cardsRef.indexOf(event.currentTarget), 1);
-        // cardsRef.splice(state.ref.indexOf(event.currentTarget), 1);
-      }
-      if (event.currentTarget.dataset.id !== state.id) {
-        console.log(0);
-        event.currentTarget.classList.add("active");
-        event.currentTarget.removeEventListener("click", compareCard);
-        state.currentTarget = event.currentTarget;
-        console.log(state.event);
-        setTimeout(repairCards.bind(state), 500);
-      }
-    } else if (state.position === 1) {
-      console.log(state);
-      console.log(event.currentTarget);
-      state.position = 2;
-      state.ref = event.currentTarget;
-      state.id = event.currentTarget.dataset.id;
-      event.currentTarget.removeEventListener("click", compareCard);
-      event.currentTarget.querySelector("img").classList.add("active");
-      event.currentTarget.classList.add("active");
-    }
-  };
-  cards.forEach((card) => card.addEventListener("click", compareCard));
-};
 gamePlay(cardsRef);
